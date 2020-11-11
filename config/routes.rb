dc5aeeccab0854760ad7d51ec260c1ecdafd675d
Capo Wang <capowangshuorui@gmail.com>
2020-11-11 18:31:29 +0800

    try-i18b

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  scope '(:locale)', locale: /en|cn/ do
 	resources :articles, only: [:index, :show]
 	resources :schools, only: [:index, :show] 
 	# do 
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
 	# end
 	resources :abouts, only: [:index]
 	root to: "articles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
+  end
 end
