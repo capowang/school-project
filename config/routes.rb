a1441571cd9927dbf57c4e14ea93b4e7ca092f50
Capo Wang <capowangshuorui@gmail.com>
2020-11-11 14:05:18 +0800

    basic-structure

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 	resources :schools, only: [:index, :show] do 
 		resources :teachers, only: [:index]
 	end
+	resources :abouts, only: [:index]
 	root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
