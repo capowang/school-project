67911435a8f3a4e683cd72ee9da698db06714880
Capo Wang <capowangshuorui@gmail.com>
2020-11-11 11:30:50 +0800

    admin-finsihed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-	resources :articles
-	resources :schools do 
-		resources :teachers, only: [:index, :new, :create]
+	resources :articles, only: [:index, :show]
+	resources :schools, only: [:index, :show] do 
+		resources :teachers, only: [:index]
 	end
-	resources :teachers, only: [:edit, :update, :destroy]
 	root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
